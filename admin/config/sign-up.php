ebd836c40a49c6ccdda50a1eebf79eed54dbe2c7
Vincent-Férié <arshey@live.fr>
2020-01-28 17:49:51 +0000

    Refont de la home page et configuration de login

---
+++ admin/config/sign-up.php
@@ -0,0 +1,150 @@
+<?php
+session_start();
+require_once('class.user.php');
+$user = new USER();
+
+if ($user->is_loggedin() != "") {
+    $user->redirect('../index.php');
+}
+
+if (isset($_POST['btn-signup'])) {
+    $uname = strip_tags($_POST['txt_uname']);
+    $umail = strip_tags($_POST['txt_umail']);
+    $upass = strip_tags($_POST['txt_upass']);
+
+    if ($uname == "") {
+        $error[] = "Renseigne le login stp";
+    } else if ($umail == "") {
+        $error[] = "Renseigne le amil stp";
+    } else if (!filter_var($umail, FILTER_VALIDATE_EMAIL)) {
+        $error[] = 'Email non valide!';
+    } else if ($upass == "") {
+        $error[] = "Renseigne le mot de passe stp!";
+    } else if (strlen($upass) < 6) {
+        $error[] = "Le mot de passe doit comporter minimum 6 chacractères";
+    } else {
+        try {
+            $stmt = $user->runQuery("SELECT user_name, user_email FROM users WHERE user_name=:uname OR user_email=:umail");
+            $stmt->execute(array(':uname' => $uname, ':umail' => $umail));
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if ($row['login'] == $uname) {
+                $error[] = "Désolé mais ce login est déja utilisé !";
+            } else if ($row['user_email'] == $umail) {
+                $error[] = "Désolé mais ce mail est déja utilisé !";
+            } else {
+                if ($user->register($uname, $umail, $upass)) {
+                    $user->redirect('../index.php');
+                }
+            }
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Inscription - Connexion | Administration Integration </title>
+
+
+
+    <link rel="stylesheet" href="../assets/css/login.css">
+    <link href="../assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="../assets/bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" media="screen">
+
+
+</head>
+
+<body>
+    <!--<div align="center">
+		<img src="img/banniere1164x200.jpg">
+	</div>-->
+
+    <hgroup>
+        <h1>Inscription | Administration</h1>
+    </hgroup>
+    <form method="post" id="login-form">
+        <div align="center">
+            <div id=error"">
+                <?php
+                if (isset($error)) {
+                    foreach ($error as $error) {
+                ?>
+                        <div class="alert alert-danger">
+                            <i class="glyphicon glyphicon-warning-sign"></i> &nbsp; <?php echo $error; ?>
+                        </div>
+                    <?php
+                    }
+                } else if (isset($_GET['joined'])) {
+                    ?>
+                    <div class="alert alert-info">
+                        <i class="glyphicon glyphicon-log-in"></i> &nbsp; Successfully registered <a href='index.php'>login</a> here
+                    </div>
+                <?php
+                }
+                ?>
+
+
+            </div>
+            <img src="../assets/img/1.png" width="150">
+        </div>
+
+
+        <br>
+        <br>
+
+
+        <div class="group">
+            <input type="text" name="txt_uname">
+            <span class="highlight"></span>
+            <span class="bar"></span>
+            <label>Login</label>
+        </div>
+
+
+        <div class="group">
+            <input type="text" name="txt_umail">
+            <span class="highlight"></span>
+            <span class="bar"></span>
+            <label>Mail</label>
+        </div>
+
+
+        <div class="group">
+            <input type="password" name="txt_upass">
+            <span class="highlight"></span>
+            <span class="bar"></span>
+            <label>Mot de Passe</label>
+        </div>
+
+
+        <button type="submit" class="button buttonBlue" name="btn-signup">
+            Inscription
+            <div class="ripples buttonRipples">
+                <span class="ripplesCircle"></span>
+            </div>
+        </button>
+        <div align="center">
+            <hr>
+            <a href="../index.php">
+                <h2>
+                    <span style="color: green">Connexion</span>
+                </h2>
+            </a>
+            <hr>
+        </div>
+
+    </form>
+
+    <BR />
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+    <script src="../assets/js/login.js"></script>
+</body>
+
+</html>
---
+++ admin/home.php
@@ -0,0 +1,227 @@
+<?php
+
+require_once("config/session.php");
+
+require_once("config/class.user.php");
+$auth_user = new USER();
+
+
+$user_id = $_SESSION['user_session'];
+
+$stmt = $auth_user->runQuery("SELECT * FROM users WHERE user_id=:user_id");
+$stmt->execute(array(":user_id" => $user_id));
+
+$userRow = $stmt->fetch(PDO::FETCH_ASSOC);
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bienvenue - <?php print($userRow['user_email']); ?></title>
+    </title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="https://v40.pingendo.com/assets/4.0.0/default/theme.css" type="text/css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-md navbar-dark bg-success">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="assets\img\logo.png" width="50">MIAIE</a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar2SupportedContent" aria-controls="navbar2SupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+            <div class="collapse navbar-collapse text-center justify-content-end" id="navbar2SupportedContent">
+                <ul class="navbar-nav">
+                    <li class="nav-item"> Bonjour <?php print($userRow['user_name']); ?> </li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right"> </ul>
+                <a href="config/logout.php?logout=true" class="btn navbar-btn ml-2 text-white btn-warning"><i class="fa d-inline fa-lg fa-user-circle-o"></i> Déconnexion</a>
+            </div>
+        </div>
+    </nav>
+    <div class="py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card">
+                        <h4 class="card-title text-center" style="color: orange"><b>A la Une</b></h4>
+                        <img class="card-img-top justify-content-center" src="assets\img\home\slider.jpg" alt="Slider">
+                        <div class="card-body justify-content-center text-center">
+                            <a href="Slider/InsertionArticle.php" class="btn btn-success">Ajouter</a>
+                            <a href="Slider/ListArticle.php" class="btn btn-success">Lister</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <h4 class="card-title text-center" style="color: orange"><b>Articles</b></h4>
+                        <img class="card-img-top" src="assets\img\home\articles.jpg" alt="Articles">
+                        <div class="card-body text-center">
+                            <a href="Plus_Infos/InsertionArticle.php" class="btn btn-success">Ajouter</a>
+                            <a href="Plus_Infos/ListArticle.php" class="btn btn-success">Lister</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <h4 class="card-title text-center" style="color: orange"><b>Archives</b></h4>
+                        <img class="card-img-top" src="assets\img\home\archives.jpg" alt="Archives">
+                        <div class="card-body text-center">
+                            <a href="Flash_Infos/InsertionArticle.php" class="btn btn-success">Ajouter</a>
+                            <a href="Flash_Infos/ListArticle.php" class="btn btn-success">Lister</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card">
+                        <h4 class="card-title text-center" style="color: orange"><b>Textes Communautaires</b></h4>
+                        <img class="card-img-top justify-content-center" src="assets\img\home\texte.jpg" alt="Textes Communautaires">
+                        <div class="card-body justify-content-center text-center">
+                            <a href="Slider/InsertionArticle.php" class="btn btn-success">Ajouter</a>
+                            <a href="Slider/ListArticle.php" class="btn btn-success">Lister</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <h4 class="card-title text-center" style="color: orange"><b>Events</b></h4>
+                        <img class="card-img-top" src="assets\img\home\events.jpg" alt="Events">
+                        <div class="card-body text-center">
+                            <a href="Plus_Infos/InsertionArticle.php" class="btn btn-success">Ajouter</a>
+                            <a href="Plus_Infos/ListArticle.php" class="btn btn-success">Lister</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <h4 class="card-title text-center" style="color: orange"><b>Bourses d'Etudes</b></h4>
+                        <img class="card-img-top" src="assets\img\home\bourses.jpg" alt="Bourses d'Etudes">
+                        <div class="card-body text-center">
+                            <a href="Flash_Infos/InsertionArticle.php" class="btn btn-success">Ajouter</a>
+                            <a href="Flash_Infos/ListArticle.php" class="btn btn-success">Lister</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card">
+                        <h4 class="card-title text-center" style="color: orange"><b>Brèves</b></h4>
+                        <img class="card-img-top" src="assets\img\home\breves.jpg" alt="brèves">
+                        <div class="card-body justify-content-center text-center">
+                            <a href="Infos_Urgent/InsertionArticle.php" class="btn btn-success">Ajouter</a>
+                            <a href="Infos_Urgent/ListArticle.php" class="btn btn-success">Lister</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <h4 class="card-title text-center" style="color: orange"><b>Magazines</b></h4>
+                        <img class="card-img-top" src="assets\img\home\magazines.jpg" alt="Magazines">
+                        <div class="card-body text-center">
+                            <a href="Offre_Emplois/InsertionArticle.php" class="btn btn-success">Ajouter</a>
+                            <a href="Offre_Emplois/ListArticle.php" class="btn btn-success">Lister</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <h4 class="card-title text-center" style="color: orange"><b>Insertion d'images</b></h4>
+                        <img class="card-img-top" src="assets\img\home\portfolio.jpg" alt="Portflolio">
+                        <div class="card-body text-center">
+                            <a href="Images\img_1.php" class="btn btn-success">Even.1</a>
+                            <a href="Images\img_2.php" class="btn btn-success">Even.2</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card">
+                        <h4 class="card-title text-center" style="color: orange"><b>Vidéos</b></h4>
+                        <img class="card-img-top" src="assets\img\home\videos.jpg" alt="Vidéos">
+                        <div class="card-body text-center">
+                            <a href="Videos/InsertionVideo.php" class="btn btn-success">Ajouter</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <h4 class="card-title text-center" style="color: orange"><b>Message Privé</b></h4>
+                        <img class="card-img-top" src="assets\img\home\messages.jpg" alt="Message">
+                        <div class="card-body text-center">
+                            <a href="Message_Prive/ListMessage.php" class="btn btn-success">Lire</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <h4 class="card-title text-center" style="color: orange"><b>Emplois</b></h4>
+                        <img class="card-img-top" src="assets\img\home\emplois.jpg" alt="Emplois">
+                        <div class="card-body text-center">
+                            <a href="Liste_Diaspo/InsertionArticle.php" class="btn btn-success">Ajouter</a>
+                            <a href="Liste_Diaspo/ListArticle.php" class="btn btn-success">Lister</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card">
+                        <h4 class="card-title text-center" style="color: orange"><b>Projets Régionaux</b></h4>
+                        <img class="card-img-top" src="assets\img\home\p-regionaux.jpg" alt="Projets Régionaux">
+                        <div class="card-body text-center">
+                            <a href="p-regionaux\add.php" class="btn btn-success">Ajouter</a>
+                            <a href="p-regionaux\index.php" class="btn btn-success">Lister</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <h4 class="card-title text-center" style="color: orange"><b>Projets Spécifiques</b></h4>
+                        <img class="card-img-top" src="assets\img\home\p-specifiques.jpg" alt="Projets Spécifiques">
+                        <div class="card-body text-center">
+                            <a href="p-specifiques\add.php" class="btn btn-success">Ajouter</a>
+                            <a href="p-specifiques\index.php" class="btn btn-success">Lister</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <h4 class="card-title text-center" style="color: orange"><b>Projets Transfontaliers</b></h4>
+                        <img class="card-img-top" src="assets\img\home\p-transfontalier.jpg" alt="Projets Transfontaliers">
+                        <div class="card-body text-center">
+                            <a href="p-transfontaliers\add.php" class="btn btn-success">Ajouter</a>
+                            <a href="p-transfontaliers\index.php" class="btn btn-success">Lister</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="text-light bg-success p-0">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 mt-3 text-center">
+                    <p>© Copyright 2018 LEVEL - All rights reserved.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
